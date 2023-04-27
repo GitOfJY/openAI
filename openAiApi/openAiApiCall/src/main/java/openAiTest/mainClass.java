b484f9549574f6e3a3166a4d5570946198791e24
JIYEON <104612045+GitOfJY@users.noreply.github.com>
2023-04-27 10:33:07 +0900

    Update mainClass.java

--- openAiApi/openAiApiCall/src/main/java/openAiTest/mainClass.java
+++ openAiApi/openAiApiCall/src/main/java/openAiTest/mainClass.java
@@ -21,99 +21,99 @@ public class mainClass {
 		HttpClient client = HttpClientBuilder.create().build();		
 
 		Scanner sc = new Scanner(System.in);
-        String startOrStop = "Y";
+        	String startOrStop = "Y";
 
-        Calendar calendar = Calendar.getInstance();
-        SimpleDateFormat formatter = new SimpleDateFormat("yyMMdd(HH-mm-ss)");
+        	Calendar calendar = Calendar.getInstance();
+        	SimpleDateFormat formatter = new SimpleDateFormat("yyMMdd(HH-mm-ss)");
 
-        String newFileName = formatter.format(calendar.getTime()).toString();
-        File file = new File("C:\\resultTxt\\"+newFileName+".txt");
-        String newFilePath = "C:\\resultTxt\\"+newFileName+".txt";
+        	String newFileName = formatter.format(calendar.getTime()).toString();
+        	File file = new File("C:\\resultTxt\\"+newFileName+".txt");
+        	String newFilePath = "C:\\resultTxt\\"+newFileName+".txt";
 
-        BufferedWriter bf = new BufferedWriter(new FileWriter(newFilePath, true));
+        	BufferedWriter bf = new BufferedWriter(new FileWriter(newFilePath, true));
 
-        System.out.println("\n\n========================================\n");
-        System.out.println("api_test 콘솔을 시작합니다.\n");
-        System.out.println("========================================\n");
+		System.out.println("\n\n========================================\n");
+		System.out.println("api_test 콘솔을 시작합니다.\n");
+		System.out.println("========================================\n");
 
-        while (startOrStop.toUpperCase().equals("Y")) {
+		while (startOrStop.toUpperCase().equals("Y")) {
 
-            System.out.println("\n1 또는 2를 입력해 주세요.");
-            System.out.println("(1: 파일 대화, 2: 사용자 입력 대화)");
+			System.out.println("\n1 또는 2를 입력해 주세요.");
+			System.out.println("(1: 파일 대화, 2: 사용자 입력 대화)");
 
-            try {
+		    	try {
 
-                int inputnum = sc.nextInt();
+				int inputnum = sc.nextInt();
 
-                switch(inputnum) {
-                    case 1: 
-                        // 파일 대화 이용
-                        System.out.println("\n\n========================================\n");
-                        System.out.println("\n파일 대화 - 콘솔을 시작합니다.\n\n");
-                        System.out.println("========================================\n");                    	
+				switch(inputnum) {
+			    		case 1: 
+					// 파일 대화 이용
+					System.out.println("\n\n========================================\n");
+					System.out.println("\n파일 대화 - 콘솔을 시작합니다.\n\n");
+					System.out.println("========================================\n");                    	
 
-                        bf.write("\n\n========================================\n");
-                        bf.write("파일 대화 - 콘솔을 시작합니다.\n");
-                        bf.write("========================================\n\n"); 
-                    	
-                        ca.callOpenAi(client, newFilePath, 1, bf);
-                        
-                        System.out.println("\n\n========================================\n");
-                        System.out.println("\n파일 대화 - 콘솔을 종료합니다.\n\n");
-                        System.out.println("========================================\n");                    	
+					bf.write("\n\n========================================\n");
+					bf.write("파일 대화 - 콘솔을 시작합니다.\n");
+					bf.write("========================================\n\n"); 
 
-                        bf.write("\n\n========================================\n");
-                        bf.write("파일 대화 - 콘솔을 종료합니다.\n");
-                        bf.write("========================================\n\n");  
-                        
-                        break;
-                        
-                    case 2:
-                        // 사용자 대화 이용
-                        System.out.println("\n\n========================================\n");
-                        System.out.println("\n사용자 대화 - 콘솔을 시작합니다.\n\n");
-                        System.out.println("========================================\n\n");                    	
+					ca.callOpenAi(client, newFilePath, 1, bf);
 
-                        bf.write("\n\n========================================\n");
-                        bf.write("사용자 대화 - 콘솔을 시작합니다.\n");
-                        bf.write("========================================\n\n"); 
-                        
-                        ca.callOpenAi(client, newFilePath, 2, bf);
-                        
-                        System.out.println("\n\n========================================\n");
-                        System.out.println("\n사용자 대화 - 콘솔을 종료합니다.\n\n");
-                        System.out.println("========================================\n\n");                    	
+					System.out.println("\n\n========================================\n");
+					System.out.println("\n파일 대화 - 콘솔을 종료합니다.\n\n");
+					System.out.println("========================================\n");                    	
 
-                        bf.write("\n\n\n========================================\n");
-                        bf.write("사용자 대화 - 콘솔을 종료합니다.\n");
-                        bf.write("========================================\n\n"); 
-                        
-                        break;
-                        
-                    default :
-                        System.out.println("\n 1 또는 2를 입력해 주세요.");
-                }
-                    
-            } catch (InputMismatchException e) {
-                System.out.println("\n정수가 아닙니다. 1 또는 2를 입력해 주세요.");
-            }
+					bf.write("\n\n========================================\n");
+					bf.write("파일 대화 - 콘솔을 종료합니다.\n");
+					bf.write("========================================\n\n");  
+
+					break;
+
+			    		case 2:
+					// 사용자 대화 이용
+					System.out.println("\n\n========================================\n");
+					System.out.println("\n사용자 대화 - 콘솔을 시작합니다.\n\n");
+					System.out.println("========================================\n\n");                    	
+
+					bf.write("\n\n========================================\n");
+					bf.write("사용자 대화 - 콘솔을 시작합니다.\n");
+					bf.write("========================================\n\n"); 
+
+					ca.callOpenAi(client, newFilePath, 2, bf);
+
+					System.out.println("\n\n========================================\n");
+					System.out.println("\n사용자 대화 - 콘솔을 종료합니다.\n\n");
+					System.out.println("========================================\n\n");                    	
+
+					bf.write("\n\n\n========================================\n");
+					bf.write("사용자 대화 - 콘솔을 종료합니다.\n");
+					bf.write("========================================\n\n"); 
+
+					break;
+
+			    		default :
+						System.out.println("\n 1 또는 2를 입력해 주세요.");
+			    		}
+
+				} catch (InputMismatchException e) {
+					System.out.println("\n정수가 아닙니다. 1 또는 2를 입력해 주세요.");
+				}
             
-            System.out.println("api_test 계속 진행하려면 Y를 입력해 주세요.");
-            sc.nextLine();
-            String input = sc.nextLine();
+		    	System.out.println("api_test 계속 진행하려면 Y를 입력해 주세요.");
+		    	sc.nextLine();
+		    	String input = sc.nextLine();
 
-            if (!input.toUpperCase().equals("Y")) {
-                startOrStop = "N";
-                sc.close();
-            }
-        }
+		    	if (!input.toUpperCase().equals("Y")) {
+				startOrStop = "N";
+				sc.close();
+			}
+        	}
 
-        bf.write("\n\n\n\n\n----------------------------------------\n");
-        bf.write(newFileName+"\n");
-        bf.write("api_test 콘솔 종료합니다.\n\n");
-        System.out.println("\n\n\n\n\n----------------------------------------\n");
-        System.out.println("api_test 콘솔 종료합니다.\n\n");
-        bf.flush();
-        bf.close();
-    }
+		bf.write("\n\n\n\n\n----------------------------------------\n");
+		bf.write(newFileName+"\n");
+		bf.write("api_test 콘솔 종료합니다.\n\n");
+		System.out.println("\n\n\n\n\n----------------------------------------\n");
+		System.out.println("api_test 콘솔 종료합니다.\n\n");
+		bf.flush();
+		bf.close();
+	}
 }
